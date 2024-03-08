da7d3112ece1e3b5b55b866cbdb113c777cd5f82
Qinkong <60113193+QINQINKONG@users.noreply.github.com>
2024-03-08 15:07:07 -0500

    Add files via upload

---
+++ setupwetbulb.py
@@ -0,0 +1,21 @@
+from setuptools import Extension, setup
+from Cython.Build import cythonize
+import numpy
+
+ext_modules = [
+    Extension(
+	"wetbulb",
+        ["wetbulb.pyx"],
+        libraries=["m"],
+        extra_compile_args=['-fopenmp','-Ofast'],
+        extra_link_args=['-fopenmp'],
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+    )
+]
+
+setup(
+    name='wetbulb',
+    ext_modules=cythonize(ext_modules,annotate=True,language_level = "3"),
+    zip_safe=False,
+    include_dirs=[numpy.get_include()]
+)
